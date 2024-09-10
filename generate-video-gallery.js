b95907c04ea11cbf415db2b6c43338d6901c9f21
GS OAR <175182661+gs-oar@users.noreply.github.com>
2024-09-10 22:39:49 +0800

    pls to help i not good at computer

--- generate-video-gallery.js
+++ generate-video-gallery.js
@@ -43,8 +43,9 @@ const createVideoPage = (videoFile) => {
 
   // Replace placeholders with actual values
   videoTemplate = videoTemplate.replace(/\{\{ video_title \}\}/g, videoTitle);
+  videoTemplate = videoTemplate.replace(/\{\{ video_element \}\}/g, videoElement);
   videoTemplate = videoTemplate.replace(/\{\{ base_url \}\}/g, baseUrl);
-  
+
   // Write the individual video HTML file
   const outputVideoFile = path.join(outputDir, `${videoFile}.html`);
   fs.writeFileSync(outputVideoFile, videoTemplate);
